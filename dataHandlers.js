2f6f59dae59076de82cfefe24d7189acf179e453
fvgm-spec <60470663+fvgm-spec@users.noreply.github.com>
2020-05-20 16:11:55 -0300

    Update dataHandlers.js

--- dataHandlers.js
+++ dataHandlers.js
@@ -1,6 +1,6 @@
 var parseDate = d3.timeParse("%m/%d/%Y");
-
-d3.csv("prices.csv")
+var urlCSV = "https://raw.githubusercontent.com/fvgm-spec/Codecademy-JS-Projects/master/prices.csv";
+d3.csv(urlCSV)
     .row(function(d){ return {month: parseDate(d.month), price:Number(d.price.trim().slice(1))}; })
     .get(function(error,data){
 
